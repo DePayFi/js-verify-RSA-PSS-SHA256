4694f46a9d1ebb3d2df9d025626ab489460a4513
Sebastian Pape <s@pape.io>
2022-08-23 11:08:30 +0200

    test with urlsafe_base64 decoded example

--- tests/units/verify.spec.js
+++ tests/units/verify.spec.js
@@ -5,8 +5,8 @@ describe('verifies signatures', () => {
   it('verifies signatures for given signature, publicKey, data', async ()=> {
 
     const data = "Sign me!"
-    const signature = "AVovd+VSNI/6bg4E4eH+VI/neFuLxREKRqjtOFAMafSOTR9m0B96bBJdAxJ8\n8raPMCkg6R80uc48kV5UsaKCTbo4W+KUHlXONS2RPJz2DCO6E8Kq9K3h7GbB\nO2PUWAD/r4zALkp8gFymyte5E/iRq8AEHTjaPtuYltIzfP7TV8sW3nhFxrTG\nBWG4/fIgb1m+KsAYD19dOSpghOvhJC/WbMJOIt4YeiyZDU9I9G+F5dl6so/m\nxm93jkUq2mJnV8vuXjqRn/KrcTFZPxNTQVSTF/oOpVjXMsPLyJqJxS7giq2u\n2Y/4qquDzmiCdqK8woqrBP13cf0+kkeYS9lmJ14fTQ==\n"
-    const publicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7Uao+6ZgMCCJmvTeOZY7\nf33PZrclAygUoVObqMka3GEGZTcHeBMxGAzs2upwjtn7OcMej7EoVGPDnPaNlgdW\nWRYXe/HG5M4yqSJWbPpuEejxVuNd8DWCKn1V0lvlVy/SCdzBaU0RCvuSiW0PoEla\nsAQr5jKr+R8ORnnU7EZlfAeol07T0AHeB1HBRNuRkBZY+KjN3eOmGMP1ClPJfLhK\nDS7pB1/lfZoPEIYdvB0r6EujKrZv88kLZXFb/OVnr/OsVEpriZlKWWWFTNaMnAHp\n330g1dVh0oEHoiz98G/sFHhr4TsQlp+avfpOlaaVNVf+WuFEIohJspj1MSHdmv6L\n3QIDAQAB\n-----END PUBLIC KEY-----\n"
+    const signature = "ilTUthI0dHMQZXpAL3thxAYwI2oSxHz0RRjCpEpc6G6gUPdVNhndc8-q0tODiPgYREUMGZtbcprphcVwfzItXNcKppv48BKq4zHqDjsZL_yqq3gCxr8s1hCNuhfcnHw7L0NntwlN7JAp_iijyKhpu74GnXwQtjrcHjvFLqjtsdeAP29DQruBGsknZKZtg8Jtrm7kvh3Ekh3BewzBYBfsu9gbvc4daYgnfeKAPRzsrd8vVjW60cL11dJjeQ7a9NaMSkszf9SVSNBas8McAAHbB19KqNmXJPnfJ9cdq-dzAVIF3Ilz83BsSA1necCIpKTb9phP4DNkcu3s5JOk3C7MEA=="
+    const publicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr3eBIMsmM+lnrt4a8ZKe\nremQjs2hBhQrkVP6rtXyIH7cHJ9QFfnML6Eo7Dd37USydHWi2irw+ZNcWiLPxlNN\nvR0IFPulCPmrIncngyy7WJ5omKYmfU9E/FD6GNt4XHJ9TlbGAzGkoAJYN6KpvVve\nfAQnNbOD5PZ3xjPSJAAAK9/gWo82GbgtKt5+hWln5ZHO82ImC+DUhmJHNPR6dSsn\nTvU5S/I+N8rycEpWCewJSCm6y58q5f7OI7dMe3l4t/YyLXqVtPiSDNgcUPKfaBsg\n/nswyGfX0mlmfFI3An4yXASh0LrUwrfsZk+x3jxvqdU7UgKOjViYWQPUTlLm2fcR\n1QIDAQAB\n-----END PUBLIC KEY-----\n"
 
     expect(
       await verify({
