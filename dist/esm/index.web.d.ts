e373d2b58023b453d60aebee6c1d8202b47bb2be
Sebastian Pape <s@pape.io>
2023-10-12 10:36:02 +0200

    v3.1.6: make hard dependencies for node package

--- dist/esm/index.web.d.ts
+++ dist/esm/index.web.d.ts
@@ -1,7 +1,7 @@
 declare function verify({
-  signature: string;
-  publicKey: string;
-  data: string | object;
+  signature: string,
+  publicKey: string,
+  data: string | object,
 }): Promise<boolean>;
 
 export {
